64a4fcc926f6714e5340f41a8ec346f17e784482
Vadim Vprodus <vproduspro@gmail.com>
2023-02-26 19:22:40 +0300

    change background color with radio button

--- lib/pragmaticstudio_web/live/light_live.ex
+++ lib/pragmaticstudio_web/live/light_live.ex
@@ -2,7 +2,7 @@ defmodule PragmaticstudioWeb.LightLive do
   use PragmaticstudioWeb, :live_view
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, brightness: 10)
+    socket = assign(socket, brightness: 10, temp: "4000")
     {:ok, socket}
   end
 
@@ -11,7 +11,7 @@ defmodule PragmaticstudioWeb.LightLive do
     <h1>Front Porch Light</h1>
     <div id="light">
       <div class="meter">
-        <span style={"width: #{@brightness}%"}>
+        <span style={"width: #{@brightness}%; background: #{temp_color(@temp)}"}>
           <%= @brightness %>%
         </span>
       </div>
@@ -34,9 +34,24 @@ defmodule PragmaticstudioWeb.LightLive do
         <input type="range" min="0" max="100"
                 name="brightness" value={@brightness} />
       </form>
+      <form phx-change="change-temp">
+        <div class="temps">
+            <%= for temp <- ["3000", "4000", "5000"] do %>
+              <div>
+                <input type="radio" id={temp} name="temp" value={temp} checked={temp == @temp} />
+                <label for={temp}><%= temp %></label>
+              </div>
+            <% end %>
+        </div>
+      </form>
     </div>
 
     """
+  end
+
+
+  def handle_event("change-temp", %{"temp" => temp}, socket) do
+    {:noreply, assign(socket, temp: temp)}
   end
 
   def handle_event("brightness", %{"brightness" => number}, socket) do
@@ -73,4 +88,8 @@ defmodule PragmaticstudioWeb.LightLive do
     socket = assign(socket, :brightness, Enum.random(0..100))
     {:noreply, socket}
   end
+
+  defp temp_color("3000"), do: "#F1C40D"
+  defp temp_color("4000"), do: "#FEFF66"
+  defp temp_color("5000"), do: "#99CCFF"
 end
